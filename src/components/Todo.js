e8e27aa51bed17d464c0cf947385712193dd9a50
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-01-17 18:22:47 +0100

    add props for completing and deleting tasks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const Todo = ({name, completed, id}) => {
+const Todo = ({name, completed, id, toggleTaskCompleted, deleteTask}) => {
     return (
         <li className = "todo stack-small">
 					<div className = "c-cb">
-						<input id = {id} type = "checkbox" defaultChecked = {completed} />
+						<input 
+							id = {id} 
+							type = "checkbox" 
+							defaultChecked = {completed} 
+							onChange = {() => toggleTaskCompleted(id)}
+							/>
 						<label className = "todo-label" htmlFor = {id}>
 							{name}
 						</label>
@@ -13,7 +18,11 @@ const Todo = ({name, completed, id}) => {
 						<button type = "button" className = "btn">
 							Edit <span className = "visually-hidden">{name}</span>
 						</button>
-						<button type = "button" className = "btn btn__danger">
+						<button 
+							type = "button" 
+							className = "btn btn__danger"
+							onClick = {() => deleteTask(id)}
+							>
 							Delete <span className = "visually-hidden">{name}</span>
 						</button>
 					</div>
