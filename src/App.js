fdae9303972fa531f6824a2ef5d5c608d67b88f9
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-01-17 19:26:22 +0100

    create object for filter buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 
+const FILTER_MAP = {
+	All: () => true,
+	Active: task => !task.completed,
+	Completed: task => task.completed
+};
+
 
 function App({theTasks}) {
 
